3afc2acbaf8f020d774a0da1c3ce86d99f2cde38
MicahJank <MicahJank@users.noreply.github.com>
2019-10-10 20:37:53 -0500

    added an edit button non functional

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -42,6 +42,9 @@ export default class Movie extends React.Component {
         <div className="save-button" onClick={this.saveMovie}>
           Save
         </div>
+        <div className="edit-button" onClick={this.saveMovie}>
+          Edit
+        </div>
       </div>
     );
   }
---
+++ client/src/Movies/UpdateMovieForm.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+
+const UpdateMovieForm = () => {
+
+    return (
+        <form>
+            <input
+                type='text'
+                name='name'
+                value=
+
+            /> 
+            <input
+                type='text'
+                name='director'
+                value=
+
+            />
+            <input
+                type='text'
+                name='score'
+                value=
+    
+            />
+            <input
+                type='text'
+                name='actor1'
+                value=
+
+            />
+            <input
+                type='text'
+                name='actor2'
+                value=
+
+            />
+            <input
+                type='text'
+                name='actor3'
+                value=
+
+            />
+        </form>
+    )
+}

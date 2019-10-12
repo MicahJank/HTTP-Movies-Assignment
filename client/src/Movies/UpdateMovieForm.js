f978731623dfefe0130904223bbdd457a2d6f0ff
MicahJank <MicahJank@users.noreply.github.com>
2019-10-12 11:40:28 -0500

    movies get populated with the data

--- client/src/Movies/UpdateMovieForm.js
+++ client/src/Movies/UpdateMovieForm.js
@@ -1,50 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const UpdateMovieForm = ({ movie, editing }) => {
 
+    const [state, setState] = useState(movie);
+
+    const handleChange = e => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+        })
+
+    }
+
+
     return (
-        <form className='movie-form'>
+        <form className={`movie-form ${editing ? 'editing' : ''}`}>
             <h2 className={!editing ? 'active' : 'hidden'}>{movie.title}</h2>
-            <input className={editing ? 'active' : 'hidden'}
+            <input className={`movie-name ${editing ? 'active' : 'hidden'}`}
                 type='text'
-                name='name'
-                value=''
+                name='title'
+                value={state.title || ''}
+                onChange={handleChange}
             />
             <div className={`movie-director ${!editing ? 'active' : 'hidden'}`}>
             Director: <em>{movie.director}</em>
             </div>
-            <input className={editing ? 'active' : 'hidden'}
+            <input className={`director-input ${editing ? 'active' : 'hidden'}`}
                 type='text'
                 name='director'
-                value=''
+                value={state.director || ''}
+                onChange={handleChange}
             />
              <div className={`movie-metascore ${!editing ? 'active' : 'hidden'}`}>
             Metascore: <strong>{movie.metascore}</strong>
             </div>
             <input className={editing ? 'active' : 'hidden'}
                 type='text'
-                name='score'
-                value=''
+                name='metascore'
+                value={state.metascore || ''}
+                onChange={handleChange}
             />
             <h3 className={!editing ? 'active' : 'hidden'}>Actors</h3>
             <label className={!editing ? 'active' : 'hidden'}>{movie.stars[0]}</label>
             <input className={editing ? 'active' : 'hidden'}
                 type='text'
-                name='actor1'
-                value=''
+                name='0'
+                value={state.stars[0] || ''}
+                onChange={handleChange}
             />
             <label className={!editing ? 'active' : 'hidden'}>{movie.stars[1]}</label>
             <input className={editing ? 'active' : 'hidden'}
                 type='text'
-                name='actor2'
-                value=''
+                name='1'
+                value={state.stars[1] || ''}
+                onChange={handleChange}
             />
             <label className={!editing ? 'active' : 'hidden'}>{movie.stars[2]}</label>
             <input className={editing ? 'active' : 'hidden'}
                 type='text'
-                name='actor3'
-                value=''
+                name='2'
+                value={state.stars[2] || ''}
+                onChange={handleChange}
             />
         </form>
     )
